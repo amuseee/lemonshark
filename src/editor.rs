72f66fc928c8e91e6b3866b476b9c6c241307835
amuseee <viksharrajesh@gmail.com>
2023-12-27 15:40:23 -0600

    we do a bit of refactoring :p

---
+++ src/editor.rs
@@ -0,0 +1,33 @@
+use std::io::{stdin, stdout, Error};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn run(&self) {
+       let _out = stdout().into_raw_mode().unwrap();
+
+        for key in stdin().keys() {
+            match key {
+                Ok(key) => match key {
+                    Key::Char(c) => {
+                    if c.is_control() {
+                         println!("{:?}\r", c as u8);
+                    } else {
+                        println!("{:?} ({})\r", c as u8, c);
+                    }
+                }
+                Key::Ctrl('q') => break,
+                _ => println!("{:?}\r", key),
+            },
+            Err(err) => diew(err),
+            }
+        } 
+    }
+}
+
+fn diew(e: Error) {
+    panic!("{}", e);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,7 @@
-use std::io::{stdin, stdout, Error};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-
-fn diew(e: Error) {
-    panic!("{}", e);
-}
+mod editor; 
+use editor::Editor;
 
 fn main() {
-    let _out = stdout().into_raw_mode().unwrap();
-
-    for key in stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?}\r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('q') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => diew(err),
-        }
-    }
+   let lemonshark = Editor{};
+   lemonshark.run(); 
 }
