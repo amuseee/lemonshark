71b5c8737ba5e19a02c39ea9251b6fff8389ad15
amuseee <viksharrajesh@gmail.com>
2023-12-26 23:26:06 -0600

    does this break it?? no it doesntclear

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::io;
-use std::io::Read;
+use std::io::{self, stdin, stdout, Read};
 
 
 fn main() {
-    for i in io::stdin().bytes() {
+    for i in stdin().bytes() {
         let c = i.unwrap() as char;
         print!("{}", c);
         if c == 'q' || c == 'Q' {
