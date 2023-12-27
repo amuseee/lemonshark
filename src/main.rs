3b2076fb46b69a0c7a667a0de23d77adec4fb100
amuseee <viksharrajesh@gmail.com>
2023-12-27 00:08:25 -0600

    exit when ctrl+q

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::io::{self, stdin, stdout, Read};
 use termion::raw::IntoRawMode;
 
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
+
+
 fn main() {
 
     let _stdout = stdout().into_raw_mode().unwrap();
@@ -9,11 +17,11 @@ fn main() {
         let i = i.unwrap();
         let c = i as char;
         if c.is_control() {
-            println!("{} \r", i);
+            println!("{:?} \r", i);
         } else {
-            println!("{} ({})\r", i, c);
+            println!("{:?} ({})\r", i, c);
         }
-        if c == 'q' {
+        if i == to_ctrl_byte('q') {
             break;
         }
     }
