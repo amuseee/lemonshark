6c43dfde4e320605afb6997dbcb2335008a9f4fd
amuseee <viksharrajesh@gmail.com>
2023-12-26 23:30:47 -0600

    termion doesnt work? even though im on wsl?

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::io::{self, stdin, stdout, Read};
-
+use termion::raw::IntoRawMode;
 
 fn main() {
     for i in stdin().bytes() {
+        
+        let _stdout = stdout().into_raw_mode().unwrap();
+        
         let c = i.unwrap() as char;
         print!("{}", c);
         if c == 'q' || c == 'Q' {
