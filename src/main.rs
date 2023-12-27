ffb275e85525fe8ba6acc1a95e4a2f71028155a0
amuseee <viksharrajesh@gmail.com>
2023-12-26 23:46:20 -0600

    woah this is cool

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,18 @@ use std::io::{self, stdin, stdout, Read};
 use termion::raw::IntoRawMode;
 
 fn main() {
-    for i in stdin().bytes() {
-        
-        let _stdout = stdout().into_raw_mode().unwrap();
-        
-        let c = i.unwrap() as char;
-        print!("{}", c);
-        if c == 'q' || c == 'Q' {
+
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+    for i in stdin().bytes() { 
+        let i = i.unwrap();
+        let c = i as char;
+        if c.is_control() {
+            println!("{} \r", i);
+        } else {
+            println!("{} ({})\r", i, c);
+        }
+        if c == 'q' {
             break;
         }
     }
